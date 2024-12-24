f9a97dbb868763523f0582dde85bd76beec7e10f
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-12-24 13:37:12 +0100

    CDK: fix conflicts
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte_cdk/sources/declarative/decoders/__init__.py
+++ airbyte_cdk/sources/declarative/decoders/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+from airbyte_cdk.sources.declarative.decoders.composite_decoder import CompositeRawDecoder
 from airbyte_cdk.sources.declarative.decoders.decoder import Decoder
 from airbyte_cdk.sources.declarative.decoders.json_decoder import (
     GzipJsonDecoder,
@@ -14,11 +15,10 @@ from airbyte_cdk.sources.declarative.decoders.pagination_decoder_decorator impor
     PaginationDecoderDecorator,
 )
 from airbyte_cdk.sources.declarative.decoders.xml_decoder import XmlDecoder
-from airbyte_cdk.sources.declarative.decoders.composite_decoder import CompositeRawDecoder
 
 __all__ = [
     "Decoder",
-"CompositeRawDecoder",
+    "CompositeRawDecoder",
     "JsonDecoder",
     "JsonlDecoder",
     "IterableDecoder",
