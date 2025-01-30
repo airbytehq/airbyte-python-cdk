1b3ae84769821b6f7a6f56f18b4a27cdbeb5428a
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2025-01-30 10:56:34 +0100

    ref: rename arguments
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte_cdk/sources/declarative/async_job/job_orchestrator.py
+++ airbyte_cdk/sources/declarative/async_job/job_orchestrator.py
@@ -482,16 +482,16 @@ class AsyncJobOrchestrator:
             and exception.failure_type == FailureType.config_error
         )
 
-    def fetch_records(self, partition: Iterable[AsyncJob]) -> Iterable[Mapping[str, Any]]:
+    def fetch_records(self, async_job_list: Iterable[AsyncJob]) -> Iterable[Mapping[str, Any]]:
         """
-        Fetches records from the given partition's jobs.
+        Fetches records from the given jobs.
 
         Args:
-            partition (AsyncPartition): The partition containing the jobs.
+            async_job_list Iterable[AsyncJob]: The list of AsyncJobs.
 
         Yields:
             Iterable[Mapping[str, Any]]: The fetched records from the jobs.
         """
-        for job in partition:
+        for job in async_job_list:
             yield from self._job_repository.fetch_records(job)
             self._job_repository.delete(job)
