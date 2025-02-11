428c6690ef96f49b2314b46ac3b1d3a69c1ce699
octavia-squidington-iii <contact@airbyte.com>
2025-02-11 20:22:08 +0000

    Auto-fix lint and format issues

--- airbyte_cdk/sources/file_based/stream/identities_stream.py
+++ airbyte_cdk/sources/file_based/stream/identities_stream.py
@@ -9,8 +9,8 @@ from airbyte_cdk.sources.file_based.config.file_based_stream_config import Prima
 from airbyte_cdk.sources.file_based.discovery_policy import AbstractDiscoveryPolicy
 from airbyte_cdk.sources.file_based.exceptions import FileBasedErrorsCollector
 from airbyte_cdk.sources.file_based.file_based_stream_reader import AbstractFileBasedStreamReader
-from airbyte_cdk.sources.streams.permissions.identities import Identities
 from airbyte_cdk.sources.streams.core import JsonSchema
+from airbyte_cdk.sources.streams.permissions.identities import Identities
 
 
 class FileIdentities(Identities):
