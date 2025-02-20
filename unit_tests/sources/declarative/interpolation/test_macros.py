2a4e7e4e42c2748cf0b5e3a1414afd0a22535622
pnilan <patrick.nilan@airbyte.io>
2025-02-19 16:32:39 -0800

    fix test

--- unit_tests/sources/declarative/interpolation/test_macros.py
+++ unit_tests/sources/declarative/interpolation/test_macros.py
@@ -123,25 +123,26 @@ def test_utc_datetime_to_local_timestamp_conversion():
 
 
 @pytest.mark.parametrize(
-    "test_name, input_value, input_format, expected_output",
+    "test_name, input_value, expected_output",
     [
-        ("test_basic_date", "2022-01-01", "%Y-%m-%d", datetime.datetime(2022, 1, 1)),
+        (
+            "test_basic_date",
+            "2022-01-14",
+            datetime.datetime(2022, 1, 14, tzinfo=datetime.timezone.utc),
+        ),
         (
             "test_datetime_with_time",
             "2022-01-01 13:45:30",
-            "%Y-%m-%d %H:%M:%S",
-            datetime.datetime(2022, 1, 1, 13, 45, 30),
+            datetime.datetime(2022, 1, 1, 13, 45, 30, tzinfo=datetime.timezone.utc),
         ),
         (
             "test_datetime_with_timezone",
             "2022-01-01T13:45:30+00:00",
-            "%Y-%m-%dT%H:%M:%S%z",
             datetime.datetime(2022, 1, 1, 13, 45, 30, tzinfo=datetime.timezone.utc),
         ),
-        ("test_datetime_custom_format", "01/Jan/2022", "%d/%b/%Y", datetime.datetime(2022, 1, 1)),
     ],
 )
-def test_str_to_datetime(test_name, input_value, input_format, expected_output):
+def test_str_to_datetime(test_name, input_value, expected_output):
     str_to_datetime_fn = macros["str_to_datetime"]
-    actual_output = str_to_datetime_fn(input_value, input_format)
+    actual_output = str_to_datetime_fn(input_value)
     assert actual_output == expected_output
