8bcc43c15f0f69933f6cd429b96a80a0ad31dc26
Maxime Carbonneau-Leclerc <3360483+maxi297@users.noreply.github.com>
2025-04-08 10:17:25 -0400

    Update airbyte_cdk/sources/declarative/interpolation/macros.py
    Co-authored-by: Aaron ("AJ") Steers <aj@airbyte.io>

--- airbyte_cdk/sources/declarative/interpolation/macros.py
+++ airbyte_cdk/sources/declarative/interpolation/macros.py
@@ -177,7 +177,8 @@ def format_datetime(
         dt_datetime = (
             datetime.datetime.strptime(dt, input_format) if input_format else str_to_datetime(dt)
         )
-    dt_datetime = dt_datetime.replace(tzinfo=pytz.utc)
+    if dt_datetime.tzinfo is None:
+        dt_datetime = dt_datetime.replace(tzinfo=pytz.utc)
     return DatetimeParser().format(dt=dt_datetime, format=format)
 
 
