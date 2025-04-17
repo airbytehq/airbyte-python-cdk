a6658e6ead091bf2dce5de0a49926c39beacfbd2
Aldo Gonzalez <aldo.gonzalez@airbyte.io>
2025-04-17 08:13:16 -0700

    connector builder: initial changes to pass file reference info to data

--- airbyte_cdk/sources/declarative/retrievers/file_uploader.py
+++ airbyte_cdk/sources/declarative/retrievers/file_uploader.py
@@ -9,6 +9,7 @@ from dataclasses import InitVar, dataclass, field
 from pathlib import Path
 from typing import Any, Mapping, Optional, Union
 
+from abc import ABC, abstractmethod
 from airbyte_cdk.models import AirbyteRecordMessageFileReference
 from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import (
@@ -24,12 +25,56 @@ from airbyte_cdk.sources.utils.files_directory import get_files_directory
 
 logger = logging.getLogger("airbyte")
 
+@dataclass
+class BaseFileUploader(ABC):
+    """
+    Base class for file uploader
+    """
+
+    @abstractmethod
+    def upload(self, record: Record) -> None:
+        """
+        Uploads the file to the specified location
+        """
+        ...
+
+class BaseFileWriter(ABC):
+    """
+    Base File writer class
+    """
+
+    @abstractmethod
+    def write(self, file_path: Path, content: bytes) -> int:
+        """
+        Writes the file to the specified location
+        """
+        ...
+
+class FileWriter(BaseFileWriter):
+
+    def write(self, file_path: Path, content: bytes) -> int:
+        """
+        Writes the file to the specified location
+        """
+        with open(str(file_path), "wb") as f:
+            f.write(content)
+
+        return file_path.stat().st_size
+
+class NoopFileWriter(BaseFileWriter):
+
+    def write(self, file_path: Path, content: bytes) -> int:
+        """
+        Noop file writer
+        """
+        return 0
 
 @dataclass
-class FileUploader:
+class FileUploader(BaseFileUploader):
     requester: Requester
     download_target_extractor: RecordExtractor
     config: Config
+    file_writer: BaseFileWriter
     parameters: InitVar[Mapping[str, Any]]
 
     filename_extractor: Optional[Union[InterpolatedString, str]] = None
@@ -77,9 +122,7 @@ class FileUploader:
             full_path = files_directory / file_relative_path
             full_path.parent.mkdir(parents=True, exist_ok=True)
 
-            with open(str(full_path), "wb") as f:
-                f.write(response.content)
-            file_size_bytes = full_path.stat().st_size
+            file_size_bytes = self.file_writer.write(full_path, content=response.content)
 
             logger.info("File uploaded successfully")
             logger.info(f"File url: {str(full_path)}")
@@ -91,3 +134,13 @@ class FileUploader:
                 source_file_relative_path=str(file_relative_path),
                 file_size_bytes=file_size_bytes,
             )
+
+
+@dataclass
+class ConnectorBuilderFileUploader(BaseFileUploader):
+    file_uploader: FileUploader
+
+    def upload(self, record: Record) -> None:
+        self.file_uploader.upload(record=record)
+        for file_reference_attribute in [file_reference_attribute for file_reference_attribute in record.file_reference.__dict__ if not file_reference_attribute.startswith('_')]:
+            record.data[file_reference_attribute] = getattr(record.file_reference, file_reference_attribute)
