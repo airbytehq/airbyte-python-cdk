7daa7f0e381846f9fedb7008274b3affcaf2bd0f
Aaron Steers <aj@airbyte.io>
2025-04-17 20:59:09 -0700

    fix docstring

--- airbyte_cdk/cli/__init__.py
+++ airbyte_cdk/cli/__init__.py
@@ -1,28 +1,9 @@
-# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
-"""Airbyte CDK CLI.
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+"""The `airbyte-cdk.cli` module provides command-line interfaces for the Airbyte CDK.
 
-The Airbyte CDK provides command-line tools for working with connectors and related resources.
+As of now, it includes the following commands:
 
+- `airbyte-cdk`: Commands for working with connectors.
+- `source-declarative-manifest`: Directly invoke the declarative manifests connector.
 
-```bash
-pip install airbyte-cdk
-
-pipx run airbyte-cdk [command]
-```
-
-
-
-- `airbyte-cdk-build`: Build connector Docker images (legacy entry point)
-- `source-declarative-manifest`: Run a declarative YAML manifest connector
-- `airbyte-cdk`: Main CLI entry point with subcommands
-
-
-The `airbyte-cdk` command includes subcommands organized by category:
-
-```bash
-airbyte-cdk image build [OPTIONS] CONNECTOR_DIR
-```
-
-
-For details on specific commands, see the documentation for each command module.
 """
