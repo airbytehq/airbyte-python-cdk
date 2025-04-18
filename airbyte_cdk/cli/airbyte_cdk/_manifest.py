5f025ba8fed752b490fb73971ff59b164d06ccef
Aaron Steers <aj@airbyte.io>
2025-04-18 16:28:27 -0700

    add manifest help text

--- airbyte_cdk/cli/airbyte_cdk/_manifest.py
+++ airbyte_cdk/cli/airbyte_cdk/_manifest.py
@@ -1,11 +1,19 @@
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
-"""Manifest related commands."""
+"""Manifest related commands.
+
+Coming soon.
+
+This module is planned to provide a command line interface (CLI) for validating
+Airbyte CDK manifests.
+"""
 
 import click
 
 
 @click.group(name="manifest")
-def manifest_cli_group() -> None:
+def manifest_cli_group(
+    help=__doc__,
+) -> None:
     """Manifest related commands."""
     pass
 
