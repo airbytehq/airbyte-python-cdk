565b7090efd14e97b8d6268f6f475317df5556c0
pnilan <patrick.nilan@airbyte.io>
2025-05-07 17:52:12 -0700

    add json string parsing to ValidateAdheresToSchema

--- airbyte_cdk/sources/declarative/validators/validate_adheres_to_schema.py
+++ airbyte_cdk/sources/declarative/validators/validate_adheres_to_schema.py
@@ -3,6 +3,7 @@
 #
 
 from dataclasses import dataclass
+import json
 from typing import Any, Mapping
 
 import jsonschema
@@ -25,6 +26,13 @@ class ValidateAdheresToSchema(ValidationStrategy):
         :param value: The value to validate
         :raises ValueError: If the value does not adhere to the schema
         """
+
+        if isinstance(value, str):
+            try:
+                value = json.loads(value)
+            except json.JSONDecodeError as e:
+                raise ValueError(f"Invalid JSON string: {value}") from e
+
         try:
             jsonschema.validate(instance=value, schema=self.schema)
         except jsonschema.ValidationError as e:
