f79bb74daf3bd7adda1421cf322bdc90fa0abe7d
octavia-squidington-iii <contact@airbyte.com>
2025-05-17 01:59:14 +0000

    Auto-fix lint and format issues

--- airbyte_cdk/sources/declarative/manifest_declarative_source.py
+++ airbyte_cdk/sources/declarative/manifest_declarative_source.py
@@ -262,7 +262,9 @@ class ManifestDeclarativeSource(DeclarativeSource):
             }
         )
 
-        stream_configs = self._stream_configs(self._source_config) + self._dynamic_stream_configs(self._source_config, config)
+        stream_configs = self._stream_configs(self._source_config) + self._dynamic_stream_configs(
+            self._source_config, config
+        )
 
         api_budget_model = self._source_config.get("api_budget")
         if api_budget_model:
