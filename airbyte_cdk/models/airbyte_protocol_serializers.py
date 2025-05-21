2def5fcfd3ba57aa8847750b9246d08535bdf86f
Aaron Steers <aj@airbyte.io>
2025-05-20 20:49:05 -0700

    only

--- airbyte_cdk/models/airbyte_protocol_serializers.py
+++ airbyte_cdk/models/airbyte_protocol_serializers.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
-"""This module is deprecated and exists for legacy compatibility.
+"""This module is deprecated and exists only for legacy compatibility.
 
 Instead of importing from this module, callers should import from
 `airbyte_cdk.models.airbyte_protocol` directly.
