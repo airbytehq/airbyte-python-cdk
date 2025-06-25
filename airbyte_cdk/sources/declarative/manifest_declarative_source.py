b7689f0c4de0ea7c1291e2be7cb142ab96ae910b
pnilan <patrick.nilan@airbyte.io>
2025-06-25 10:58:29 -0700

    chore: format

--- airbyte_cdk/sources/declarative/manifest_declarative_source.py
+++ airbyte_cdk/sources/declarative/manifest_declarative_source.py
@@ -238,8 +238,7 @@ class ManifestDeclarativeSource(DeclarativeSource):
         self._spec_component.transform_config(mutable_config)
         return mutable_config
 
-    def configure(self, config: Mapping[str, Any], temp_dir: str
-    ) -> Mapping[str, Any]:
+    def configure(self, config: Mapping[str, Any], temp_dir: str) -> Mapping[str, Any]:
         config = self._config or config
         return super().configure(config, temp_dir)
 
