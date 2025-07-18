2743999ccbd5ae9b4c748ca47c39a3ee367d8ea5
pnilan <patrick.nilan@airbyte.io>
2025-07-18 13:51:35 -0700

    chore: format

--- airbyte_cdk/test/entrypoint_wrapper.py
+++ airbyte_cdk/test/entrypoint_wrapper.py
@@ -68,9 +68,8 @@ class AirbyteEntrypointException(Exception):
     message: str = ""
 
     def __post_init__(self) -> None:
-        # Propagate the message to Exception so that `str(self)` and `.args`
-        # behave like a regular exception.
         super().__init__(self.message)
+
 
 class EntrypointOutput:
     """A class to encapsulate the output of an Airbyte connector's execution.
