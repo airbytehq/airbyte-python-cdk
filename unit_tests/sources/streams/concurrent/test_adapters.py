2d1e2f43396cabe792a9101af3841b5e7acc79ce
maxime.c <maxime@airbyte.io>
2025-07-31 12:08:24 -0400

    remove unused file

--- unit_tests/sources/streams/concurrent/test_adapters.py
+++ unit_tests/sources/streams/concurrent/test_adapters.py
@@ -22,11 +22,6 @@ from airbyte_cdk.sources.streams.concurrent.adapters import (
     StreamPartition,
     StreamPartitionGenerator,
 )
-from airbyte_cdk.sources.streams.concurrent.availability_strategy import (
-    STREAM_AVAILABLE,
-    StreamAvailable,
-    StreamUnavailable,
-)
 from airbyte_cdk.sources.streams.concurrent.cursor import Cursor
 from airbyte_cdk.sources.streams.concurrent.exceptions import ExceptionWithDisplayMessage
 from airbyte_cdk.sources.streams.core import Stream
