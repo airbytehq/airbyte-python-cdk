1d84a49637e9fd18fe3ce3e06198aab1c40bcf51
maxime.c <maxime@airbyte.io>
2025-08-05 10:35:42 -0400

    mypy

--- airbyte_cdk/sources/declarative/parsers/model_to_component_factory.py
+++ airbyte_cdk/sources/declarative/parsers/model_to_component_factory.py
@@ -2069,7 +2069,7 @@ class ModelToComponentFactory:
             # We specifically exclude Connector Builder stuff for now as Brian is working on this anyway
 
             stream_name = model.name or ""
-            stream_slicer: ConcurrentStreamSlicer = concurrent_cursor
+            stream_slicer: ConcurrentStreamSlicer = concurrent_cursor if concurrent_cursor else SinglePartitionRouter()
             cursor: Cursor = FinalStateCursor(stream_name, None, self._message_repository)
             if isinstance(retriever, AsyncRetriever):
                 # The AsyncRetriever only ever worked with a cursor from the concurrent package. Hence, the method
