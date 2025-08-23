704da6b1b6e48f152a835401f9158f3bcecc648e
Pedro S. Lopez <pedroslopez@me.com>
2025-08-22 23:29:45 -0400

    move cli

--- airbyte_cdk/manifest_server/cli/__init__.py
+++ airbyte_cdk/manifest_server/cli/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
-"""The `airbyte-cdk.cli.manifest_server` module provides a standalone CLI for the Airbyte CDK Manifest Server.
+"""The `airbyte_cdk.manifest_server.cli` module provides a standalone CLI for the Airbyte CDK Manifest Server.
 
 This CLI enables running a FastAPI server for managing and executing Airbyte declarative manifests.
 """
