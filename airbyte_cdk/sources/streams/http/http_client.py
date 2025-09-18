8c745d7d18562490b64ea72a1312bd9010e7b3c1
Mieszko Boczkowski <mieszko@bosper.pl>
2025-09-18 16:35:41 +0200

    Fix tests

--- airbyte_cdk/sources/streams/http/http_client.py
+++ airbyte_cdk/sources/streams/http/http_client.py
@@ -545,7 +545,13 @@ class HttpClient:
             data=data,
         )
 
-        env_settings = self._session.merge_environment_settings(request.url, None, None, None, None)
+        env_settings = self._session.merge_environment_settings(
+            url=request.url,
+            proxies=request_kwargs.get("proxies"),
+            stream=request_kwargs.get("stream"),
+            verify=request_kwargs.get("verify"),
+            cert=request_kwargs.get("cert"),
+        )
         request_kwargs = {**request_kwargs, **env_settings}
 
         response: requests.Response = self._send_with_retry(
