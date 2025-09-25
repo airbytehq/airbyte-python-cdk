9df9c675307524f3032ebc381181a715915accd1
ChristoGrab <christo.grab@gmail.com>
2025-09-25 14:38:37 -0700

    chore: format

--- airbyte_cdk/manifest_server/routers/manifest.py
+++ airbyte_cdk/manifest_server/routers/manifest.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, List, Mapping, Optional
 import jsonschema
 from airbyte_protocol_dataclasses.models import AirbyteStateMessage, ConfiguredAirbyteCatalog
 from fastapi import APIRouter, Depends, HTTPException
+
 from airbyte_cdk.models import AirbyteStateMessageSerializer
 from airbyte_cdk.sources.declarative.concurrent_declarative_source import (
     ConcurrentDeclarativeSource,
