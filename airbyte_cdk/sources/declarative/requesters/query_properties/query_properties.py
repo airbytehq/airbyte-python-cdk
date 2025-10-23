d22170d61461634994c430eb2553a756c176407a
maxime.c <maxime@airbyte.io>
2025-10-23 15:28:48 -0400

    update doc

--- airbyte_cdk/sources/declarative/requesters/query_properties/query_properties.py
+++ airbyte_cdk/sources/declarative/requesters/query_properties/query_properties.py
@@ -29,8 +29,6 @@ class QueryProperties:
         """
         Uses the defined property_list to fetch the total set of properties dynamically or from a static list
         and based on the resulting properties, performs property chunking if applicable.
-        :param stream_slice: The StreamSlice of the current partition being processed during the sync. This is included
-        because subcomponents of QueryProperties can make use of interpolation of the top-level StreamSlice object
         """
         fields: List[str]
         if isinstance(self.property_list, PropertiesFromEndpoint):
